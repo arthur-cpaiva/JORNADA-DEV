4b2d320c1ee6fa9c5f0330527694af570674f5e6
Arthur Paiva <arthur-68@hotmail.com>
2026-05-15 21:21:31 -0300

    Aplicando detalhes ao CSS e adicionando o [function] ao JS.

---
+++ script.js
@@ -0,0 +1,9 @@
+function acessar() {
+
+    { window.location.href = "https://github.com/arthur-cpaiva" };
+
+}
+
+function feedback() {
+    { window.location.href = "https://linkdedin.com/in/arthur-da-cunha-paiva" };
+}
